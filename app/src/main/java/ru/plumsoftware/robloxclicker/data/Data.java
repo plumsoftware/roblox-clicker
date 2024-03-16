5935977a2142408787f6655f2269af25949a4b1c
Slava <plumsoftwareofficial@gmail.com>
2024-03-16 23:23:38 +0600

    [+] Добавил скибиди сэт [^] Апнул грэйдл и либы

--- app/src/main/java/ru/plumsoftware/robloxclicker/data/Data.java
+++ app/src/main/java/ru/plumsoftware/robloxclicker/data/Data.java
@@ -29,13 +29,15 @@ public abstract class Data {
             "ci",
             "ame",
             "darkm",
-            "atow"
+            "atow",
+            "skibidi"
     };
 
     public static final String[] SP_BACK_IS_BUY = new String[]{
             "back_2",
             "back_3",
             "back_4",
-            "back_5"
+            "back_5",
+            "back_6"
     };
 }
